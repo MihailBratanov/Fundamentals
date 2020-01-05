4f4da9327ae87707bc20ca789aec70f491e2175d
mirozspace <zstefchev@gmail.com>
2020-01-05 14:49:38 +0200

    add new line

--- src/twelfth.java
+++ src/twelfth.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
 public class twelfth {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
         int i = 0;
-        while (n%2!=0){
+        while (n % 2 != 0) {
             System.out.println("Please write an even number.");
             n = Integer.parseInt(scanner.nextLine());
         }
         i = Math.abs(n);
-        System.out.printf("The number is: %d",i);
+        System.out.printf("The number is: %d", i);
+
+        //new Line
     }
 }
