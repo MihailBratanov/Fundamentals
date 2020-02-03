45536a8ff5acdbb5eec24aa011c854a5a65aab60
MihailBratanov <m.d.bratanov@gmail.com>
2020-02-03 12:27:53 +0000

    fixed exception

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,34 +1,58 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class VendingMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
-        boolean Money = true;
+
+        //name variables using camelCase  - https://en.wikipedia.org/wiki/Camel_case
+        boolean money = true;
         double sum = 0.0;
         double coins = 0;
         double cost = 0.0;
         boolean rich = true;
         boolean product = true;
-        while(!command.equals("Start")) {
+        boolean shouldPrintMoneyAccept = true;
+
+
+
+        while (!command.equals("Start")) {
             switch (command) {
+                //comment out to see what's printing if you need to debug
                 case "0.1":
+                    //System.out.println("0.1");
                 case "0.2":
+                    // System.out.println("0.2");
                 case "0.5":
+                    // System.out.println("0.5");
                 case "1":
+                    // System.out.println("1.0");
                 case "2":
-                    Money = true;
+                    money = true;
                     break;
                 default:
-                    Money = false;
+                    money = false;
                     break;
             }
-            coins = Double.parseDouble(command);
-            if (!Money) {
+            try {
+
+                coins = Double.parseDouble(command);
+                shouldPrintMoneyAccept = true;
+            } catch (NumberFormatException e) {
+                System.out.println("please enter a valid command.");
+                shouldPrintMoneyAccept = false;
+            }
+
+            if (!money && shouldPrintMoneyAccept) {
+
                 System.out.printf("Cannot accept %.2f.", coins);
                 System.out.println();
             } else {
                 sum += coins;
+                // System.out.println(sum);
             }
             command = scanner.nextLine();
         }
@@ -56,19 +80,18 @@ public class VendingMachine {
                     product = false;
                     break;
             }
-            if(product){
-            if (sum < 0) {
-                System.out.println("Sorry, not enough money");
-                rich = false;
-            } else {
-                System.out.println(String.format("Purchased %s.", command));
-            }
+            if (product) {
+                if (sum < 0) {
+                    System.out.println("Sorry, not enough money");
+                    rich = false;
+                } else {
+                    System.out.println(String.format("Purchased %s.", command));
+                }
                 command = scanner.nextLine();
             }
         }
-            System.out.println(String.format("Change: %.2f.", sum));
-        }
-
-
+        System.out.println(String.format("Change: %.2f.", sum));
     }
 
+
+}
